d4471bc544a4308a3195a5ab725e5423f22a255a
MrTup <56383520+MrTup1@users.noreply.github.com>
2023-03-23 09:37:39 +0000

    Bug Fixes

--- script.js
+++ script.js
@@ -15,18 +15,35 @@ function sizeAdjust() {
   let banner = document.querySelector("#introBanner");
   let banner2 = document.querySelector("#aboutBanner");
   let pageTitle = document.querySelector("#pageTitle");
-
   if (height >= 870) {
     header.style.height = "12vh";
-    footer.style.height = "12vh";
-    banner.style.height = "24vh";
-    pageTitle.style.top = "15vh";
+    footer.style.height = "13vh";
   }
 
   if (height >= 972) {
     header.style.height = "10vh";
-    footer.style.height = "8vh";
-    banner.style.height = "20vh";
-    pageTitle.style.top = "12.5vh";
+    footer.style.height = "10vh";
+  }
+
+  if (banner != null) {
+    if (height >= 870) {
+      banner.style.height = "24vh";
+      pageTitle.style.top = "14vh";
+    }
+
+    if (height >= 972) {
+      banner.style.height = "20vh";
+      pageTitle.style.top = "12vh";
+    }
+  } else if (banner2 != null) {
+    if (height >= 870) {
+      banner2.style.height = "24vh";
+      pageTitle.style.top = "14vh";
+    }
+
+    if (height >= 972) {
+      banner2.style.height = "20vh";
+      pageTitle.style.top = "12vh";
+    }
   }
 }
